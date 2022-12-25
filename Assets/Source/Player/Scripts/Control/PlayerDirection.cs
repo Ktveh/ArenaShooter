4faa8321e30710cab58bfd00b63835d8c09a1c4b
Andrey Hoderev <HoderevAV@gmail.com>
2022-12-25 10:46:57 +0400

    Removed unnecessary

--- Assets/Source/Player/Scripts/Control/PlayerDirection.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class PlayerDirection : MonoBehaviour
-{
-    private void Update()
-    {
-        transform.eulerAngles = new Vector3(transform.rotation.x, Camera.main.transform.eulerAngles.y);
-    }
-}
