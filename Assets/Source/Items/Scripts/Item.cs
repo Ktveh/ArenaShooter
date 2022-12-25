201a5a3e492f4a17292e36aca7be56f619f119ca
Andrey Hoderev <HoderevAV@gmail.com>
2022-12-25 09:41:54 +0400

    Create items

---
+++ Assets/Source/Items/Scripts/Item.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private Types _type;
+    [SerializeField] private uint _amount;
+
+    public Types Type => _type;
+    public uint Amount => _amount;
+
+    public enum Types
+    {
+        SMG,
+        Rifle,
+        SniperRifle,
+        Shotgun,
+        Grenade,
+        Drug
+    }
+}
