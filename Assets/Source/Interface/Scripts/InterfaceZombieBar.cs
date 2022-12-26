4fb49ed986dec9ce08a27a4370b3767909bc48b2
Andrey Hoderev <HoderevAV@gmail.com>
2022-12-27 00:32:16 +0400

    Added zombie bar

---
+++ Assets/Source/Interface/Scripts/InterfaceZombieBar.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class InterfaceZombieBar : MonoBehaviour
+{
+    [SerializeField] private ZombieCounter _zombieCounter;
+
+    private Zombie[] _zombies;
+    private Slider _slider;
+    private TMP_Text _showingAmount;
+    private int _amountDead;
+
+    private void Awake()
+    {
+        _zombies = _zombieCounter.GetComponentsInChildren<Zombie>();
+        _slider = GetComponent<Slider>();
+        _showingAmount = GetComponentInChildren<TMP_Text>();
+        _slider.maxValue = _zombies.Length;
+        _showingAmount.text = _amountDead.ToString() + " / " + _slider.maxValue.ToString();
+    }
+
+    private void OnEnable()
+    {
+        foreach (Zombie zombie in _zombies)
+            zombie.Dead += OnDead;
+    }
+
+    private void OnDisable()
+    {
+        foreach (Zombie zombie in _zombies)
+            zombie.Dead -= OnDead;
+    }
+
+    private void OnDead(Zombie zombie)
+    {
+        _slider.value = ++_amountDead;
+        _showingAmount.text = _amountDead.ToString() + " / " + _slider.maxValue.ToString();
+    }
+}
