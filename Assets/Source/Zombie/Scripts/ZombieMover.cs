dd8eccabd286514a81eb8620b4a3f22c838c68eb
Eugene Ratushnyak <lemuriyskiy@rambler.ru>
2023-01-18 09:38:33 +0300

    Fix move after dead

--- Assets/Source/Zombie/Scripts/ZombieMover.cs
+++ Assets/Source/Zombie/Scripts/ZombieMover.cs
@@ -22,25 +22,26 @@ public class ZombieMover : MonoBehaviour
         if (_target != null)
         {
             _agent.destination = _target.transform.position;
-            if (_targeter.IsAttentive && _zombie.HasLegs && !_zombie.IsDead)
+
+            if (_targeter.IsAttentive && _zombie.HasLegs)
             {
                 _agent.speed = SetSpeed(_runSpeed);
                 _animator.Run();
             }
-            else if (_zombie.HasLegs && !_zombie.IsDead)
+            else if (_zombie.HasLegs)
             {
                 _agent.speed = SetSpeed(_walkSpeed);
                 _animator.Walk();
             }
-            else if (!_zombie.IsDead)
+            else
             {
                 _agent.speed = SetSpeed(_crawlSpeed);
                 _animator.Crawl();
             }
-            else if (_zombie.IsDead)
-            {
-                _agent.speed = SetSpeed(0);
-            }
+        }
+        if (_zombie.IsDead)
+        {
+            _agent.speed = SetSpeed(0);
         }
         if (!_agent.hasPath)
         {
