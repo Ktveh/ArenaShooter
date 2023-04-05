1c1a8026fbca415d931cea6075273757af5985ac
Eugene Ratushnyak <lemuriyskiy@rambler.ru>
2023-04-05 11:15:16 +0300

    Change Title UI

--- Assets/Source/TitleMenu/Scripts/Level.cs
+++ Assets/Source/TitleMenu/Scripts/Level.cs
@@ -7,12 +7,14 @@ public class Level : MonoBehaviour
     [SerializeField] private string _name;
     [SerializeField] private int _scene;
     [SerializeField] private bool _isLock;
+    [SerializeField] private Sprite _icon;
 
     public static string SaveKey = "Levels";
 
     public string Name => _name;
     public int Scene => _scene;
     public bool IsLock => _isLock;
+    public Sprite Icon => _icon;
 
     private void Awake()
     {
