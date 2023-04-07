25871494018f54bd0d0e4298aca127b6cad0761c
Eugene Ratushnyak <lemuriyskiy@rambler.ru>
2023-04-07 14:37:30 +0300

    Fix button

--- Assets/Source/Leaderboard/Scripts/LeaderboardButton.cs
+++ Assets/Source/Leaderboard/Scripts/LeaderboardButton.cs
@@ -6,6 +6,7 @@ using TMPro;
 
 public class LeaderboardButton : MonoBehaviour
 {
+    [SerializeField] private LeaderBoard _leaderboard;
     [SerializeField] private TextMeshProUGUI _buttonText;
 
     private const string Authorization = "Authorization";
@@ -16,5 +17,19 @@ public class LeaderboardButton : MonoBehaviour
         _buttonText.text = Lean.Localization.LeanLocalization.GetTranslationText(Authorization);
         if (PlayerAccount.IsAuthorized)
             _buttonText.text = Lean.Localization.LeanLocalization.GetTranslationText(TopPlayers);
+    }
+
+    public void OnClick()
+    {
+        if (!PlayerAccount.IsAuthorized)
+        {
+            PlayerAccount.Authorize(Rename);
+            _leaderboard.gameObject.SetActive(true);
+        }
+    }
+
+    private void Rename()
+    {
+        _buttonText.text = Lean.Localization.LeanLocalization.GetTranslationText(TopPlayers);
     }
 }
